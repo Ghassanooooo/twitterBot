e14f850cac1cc5158c82766fdad906777d86c93c
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-07-10 19:52:42 +0200

    tw

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports= {
+    consumer_key:         'q0EXkbAJXkXYIHqe5MQGbzBVi',
+    consumer_secret:      '2o9mBjnfXpP4g5C3CXqCHsj0srcKcNX7oPujTxjSz9aKiPi4w7',
+    access_token:         '3081489468-tuUj62xYux7LPzIuWxnGU47xIZ61lZYiafMgB5t',
+    access_token_secret:  'ODR7o1uwdzgNZTeiYF0hnHWnnOLTx8d6iFlPLdhKgTi02'
+ //   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+ //   strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+const Twit = require('twit');
+
+const config = require('./config');
+
+ let T = new Twit(config);
+/* get twittes */ 
+//  let param= { q: 'AFD',count: 2}
+
+//  function gotData(err, data, response) {
+//      for(let twit of data.statuses){ console.log(twit.text) } }
+
+//  T.get('search/tweets',param, gotData)
+
+/* post twittes*/
+function twites(text){
+// let rand=Math.floor(Math.random()*100)
+// let pos= { status: `${rand} #Hi_Im_Node_App` }
+
+let pos= { status: text }
+
+    function posted(err, data, response) {
+       if(err){console.log('ERROR:there is somthing wrong')}else{console.log('its work.....')}
+      }
+    
+    T.post('statuses/update',pos, posted)
+}
+// twites()
+
+// setInterval(twites,50000)
+
+var stream = T.stream('user')
+
+stream.on('follow',followed)
+function followed (event) {
+   let name=event.source.name;
+   let screenName=event.source.screen_name;
+    twites(`@${screenName} thank you for followed me`)
+  }
+
+  
+
+
